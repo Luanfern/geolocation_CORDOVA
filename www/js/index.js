c08c4141319f15fbcc39a84f102e8e41f8593825
LuanPegador07 <51713644+Luanfern@users.noreply.github.com>
2021-11-10 08:05:09 -0300

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,47 @@
+var app = {
+
+    initialize: function(){
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);    
+    },
+    onDeviceReady: function(){
+        alert("onDeviceReady: function() {...");
+        this.receivedEvent('deviceready');
+        navigator.geolocation.getCurrentPosition(
+            this.onGeolocationSuccess,
+            this.onGeolocationError,
+            {timeout: 120000}
+        );
+    },
+
+    receivedEvent: function (id) {  
+    },
+
+    onGeolocationSuccess: function(position){
+        alert("onGeolocationSuccess: function(position){...");
+        alert('Latitude: ' + position.coords.latitude + '\n' +
+        'Longitude: ' + position.coords.longitude + '\n' +
+        'Altitude: ' + position.coords.altitude + '\n' +
+        'Accuracy: ' + position.coords.accuracy + '\n' +
+        'Altitude Accuracy: ' + position.coords.altitudeaccuracy + '\n' +
+        'Heading: ' + position.coords.heading + '\n' +
+        'Speed: ' + position.coords.speed + '\n' +
+        'Timestamp: ' + position.coords.timestamp + '\n');
+    },
+
+    onGeolocationError: function(err){
+        alert("onGeolocationError: function(position){...");
+        alert(err.code + " "+ err.message);
+    }
+}
+
+app.initialize();
+
+/* document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
+ */
